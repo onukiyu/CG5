8fd5271f8f6ad4bb7f99049ff31bce74da904b3b
onukiyu <k023g0017@g.neec.ac.jp>
2025-07-23 15:21:50 +0900

    00_10_完了

---
+++ DirectXGame/WorldTransformEx.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "KamataEngine.h"   // WorldTransformクラスの利用
+class WorldTransformEx : public KamataEngine::WorldTransform {
+
+public:
+	// Affine変換行列の生成と定数バッファへの転送を行う
+	void UpdateMatrix();
+
+	// Affine変換行列の生成
+	KamataEngine::Matrix4x4 MakeAffineMatrix();
+};
--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,6 +4,7 @@
 #include "PipelineState.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "WorldTransformEx.h"
 
 #include <Windows.h>
 #include <cassert>
@@ -212,6 +213,18 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		srvHandleCPU                     // SRV用ディスクリプタヒープの CPU Handle
 	);
 
+	// アプリで利用する3Dモデル ================================
+	// 被写体の準備
+	Model* model = Model::CreateFromOBJ("terrain");
+
+	WorldTransformEx worldTransform;
+	worldTransform.Initialize();
+	worldTransform.scale_ = Vector3(1.0f, 1.0f, 1.0f);
+
+	// カメラの準備
+	Camera camera;
+	camera.Initialize();
+	camera.translation_ = Vector3(0.0f, 1.0f, 0.0f);
 
 	//メインループ
 	while (true) {
@@ -220,7 +233,12 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 			break;
 		}
 
-		// 描画開始
+		// world変換行列の定数バッファへの転送
+		worldTransform.rotation_.y += 0.005f;    // 適当な回転角度(ラジアン)
+		worldTransform.UpdateMatrix();
+
+		// cameraの更新と定数バッファへの転送
+		camera.UpdateMatrix();
 
 		// TransitionBarrierを SRV => RTV に設定する
 		D3D12_RESOURCE_BARRIER barrier{};
@@ -262,7 +280,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 		// 描画
 
-
+		Model::PreDraw(commandList);
+		model->Draw(worldTransform, camera);
+		Model::PostDraw();
 
 		// TransitionBarrierを元に戻し、PixelShaderが扱えるようにする
 		barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;  // TranslationBarrierの設定
@@ -298,6 +318,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	}
 
 	// 解放処理
+
+	delete model;
+
 	renderTextureResource->Release();
 	srvDescriptorHeap->Release();
 	rtvDescriptorHeap->Release();
