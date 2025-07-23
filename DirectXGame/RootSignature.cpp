5ffb475ae55c5231696c620735b4fe8d3c7beb66
onukiyu <k023g0017@g.neec.ac.jp>
2025-07-23 14:40:07 +0900

    00_09_完了

--- DirectXGame/RootSignature.cpp
+++ DirectXGame/RootSignature.cpp
@@ -19,6 +19,40 @@ void RootSignature::Create() {
 	// 構造的にデータを用意する
 	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
 	descriptionRootSignature.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+	// デスクリプタレンジ
+	D3D12_DESCRIPTOR_RANGE srvDescRange[1]{};
+	// t0 レジスタを利用可能にする
+	srvDescRange[0].BaseShaderRegister = 0;                         // 0から始まる
+	srvDescRange[0].NumDescriptors = 1;                             // 数は 1つ
+	srvDescRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;    // SRV
+	srvDescRange[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+	// RootParametorの用意 ※PixelShaderに読ませるために必要　★00_09 追加
+	// 複数設定できるので配列の構造をしている。今回は 1つだけなので、長さ1の配列として用意する
+	D3D12_ROOT_PARAMETER rootParameters[1]{};
+
+	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE; //DescriptorTable
+	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;           //PixelShaderで使う
+	rootParameters[0].DescriptorTable.pDescriptorRanges = srvDescRange;           // 拡張しやすくなる
+	rootParameters[0].DescriptorTable.NumDescriptorRanges = _countof(srvDescRange); // RangeTable数
+
+	descriptionRootSignature.pParameters = rootParameters;     // ルートパラメータ配列へのポインタ
+	descriptionRootSignature.NumParameters = _countof(rootParameters);  // 配列の長さ
+
+	// Sampleの設定　★00_09 追加
+	D3D12_STATIC_SAMPLER_DESC staticSamplers[1] = {};
+	staticSamplers[0].Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;     // バイリニアフィルタ
+	staticSamplers[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;   // 0.0~1.0の範囲外をリピート
+	staticSamplers[0].AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;   // 0.0~1.0の範囲外をリピート
+	staticSamplers[0].AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;   // 0.0~1.0の範囲外をリピート
+	staticSamplers[0].ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER; // 比較しない
+	staticSamplers[0].MaxLOD = D3D12_FLOAT32_MAX;                   // ありったけのMipMapを使う
+	staticSamplers[0].ShaderRegister = 0;                           // レジスタ番号 0を使う(s0)
+	staticSamplers[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL; // PixelShaderで使う
+
+	descriptionRootSignature.pStaticSamplers = staticSamplers;
+	descriptionRootSignature.NumStaticSamplers = _countof(staticSamplers);
 	ID3DBlob* signatureBlob = nullptr;
 	ID3DBlob* errorBlog = nullptr;
 	HRESULT hr = D3D12SerializeRootSignature(&descriptionRootSignature, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlog);
--- DirectXGame/VertexBuffer.cpp
+++ DirectXGame/VertexBuffer.cpp
@@ -31,7 +31,7 @@ void VertexBuffer::Create(const UINT size, const UINT stride) {
 	ID3D12Resource* vertexResource = nullptr;
 
 	// HRESULT 追加
-	HRESULT hr =
+	[[maybe_unused]] HRESULT hr =
 	    dxCommon->GetDevice()->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
 	assert(SUCCEEDED(hr)); // うまくいかなかったときは起動できない
 
